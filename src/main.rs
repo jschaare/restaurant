bae7052a7f89b4f0670ce11b1cc6c2a5b1e8e00b
Justin Schaare <jschaare16@gmail.com>
2022-06-13 02:29:17 -0400

    add routes, create test order struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,39 @@
-use actix_web::{get, middleware, web, App, HttpServer, Responder};
+use actix_web::{delete, get, middleware, post, web, App, HttpResponse, HttpServer, Responder};
+use serde::{Deserialize, Serialize};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
     format!("Hello {name}!")
+}
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct OrderTest {
+    item_name: String,
+    table_id: i32,
+}
+
+#[post("/order")]
+async fn create_order(item: web::Json<OrderTest>) -> impl Responder {
+    //format!("{:?}", item)
+    HttpResponse::Ok().json(item)
+}
+
+#[get("/order")]
+async fn get_order(item: web::Json<OrderTest>) -> impl Responder {
+    //format!("{:?}", item)
+    HttpResponse::Ok().json(item)
+}
+
+#[delete("/order")]
+async fn del_order(item: web::Json<OrderTest>) -> impl Responder {
+    //format!("{:?}", item)
+    HttpResponse::Ok().json(item)
+}
+
+#[get("/table")]
+async fn get_table_orders(item: web::Json<OrderTest>) -> impl Responder {
+    //format!("{:?}", item)
+    HttpResponse::Ok().json(item)
 }
 
 #[actix_web::main] // or #[tokio::main]
@@ -13,8 +44,11 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
-            .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(greet)
+            .service(create_order)
+            .service(get_order)
+            .service(del_order)
+            .service(get_table_orders)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
