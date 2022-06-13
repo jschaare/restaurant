3163c29fec22431f2ecb4e06a656b543c2c3d709
Justin Schaare <jschaare16@gmail.com>
2022-06-13 03:13:32 -0400

    create more basic models

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,37 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize)]
-pub struct OrderTest {
+#[derive(Debug, Deserialize)]
+pub struct OrderCreate {
     item_name: String,
     table_id: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct OrderInput {
+    id: i32,
+    table_id: i32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct TableInput {
+    table_id: i32,
+}
+
+#[derive(Debug, Serialize)]
+pub struct Order {
+    id: i32,
+    item_name: String,
+    table_id: i32,
+    cook_time: i32,
+}
+
+impl Order {
+    pub fn from_create(input: OrderCreate, id: i32, cook_time: i32) -> Order {
+        Order {
+            id: id,
+            item_name: input.item_name,
+            table_id: input.table_id,
+            cook_time: cook_time,
+        }
+    }
 }
--- src/routes.rs
+++ src/routes.rs
@@ -1,27 +1,27 @@
 use actix_web::{delete, get, post, web, HttpResponse, Responder};
+use rand::Rng;
 
-use crate::models::OrderTest;
+use crate::models::{Order, OrderCreate, OrderInput, TableInput};
 
 #[post("/order")]
-pub async fn create_order(item: web::Json<OrderTest>) -> impl Responder {
-    //format!("{:?}", item)
-    HttpResponse::Ok().json(item)
+pub async fn create_order(item: web::Json<OrderCreate>) -> impl Responder {
+    let item = item.into_inner();
+    let mut rng = rand::thread_rng();
+    let cook_time: i32 = rng.gen_range(5..15);
+    HttpResponse::Ok().json(Order::from_create(item, 0, cook_time))
 }
 
 #[get("/order")]
-pub async fn get_order(item: web::Json<OrderTest>) -> impl Responder {
-    //format!("{:?}", item)
-    HttpResponse::Ok().json(item)
+pub async fn get_order(_item: web::Json<OrderInput>) -> impl Responder {
+    HttpResponse::Ok().json({})
 }
 
 #[delete("/order")]
-pub async fn del_order(item: web::Json<OrderTest>) -> impl Responder {
-    //format!("{:?}", item)
-    HttpResponse::Ok().json(item)
+pub async fn del_order(_item: web::Json<OrderInput>) -> impl Responder {
+    HttpResponse::Ok().json({})
 }
 
 #[get("/table")]
-pub async fn get_table_orders(item: web::Json<OrderTest>) -> impl Responder {
-    //format!("{:?}", item)
-    HttpResponse::Ok().json(item)
+pub async fn get_table_orders(_item: web::Json<TableInput>) -> impl Responder {
+    HttpResponse::Ok().json({})
 }
