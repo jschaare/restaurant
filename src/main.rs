0eef842067612d9cd689e9d4860945920b777e8d
Justin Schaare <jschaare16@gmail.com>
2022-06-13 02:14:08 -0400

    add logging middleware

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web::{get, middleware, web, App, HttpServer, Responder};
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
@@ -7,8 +7,12 @@ async fn greet(name: web::Path<String>) -> impl Responder {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+
     HttpServer::new(|| {
         App::new()
+            .wrap(middleware::Logger::default())
             .route("/hello", web::get().to(|| async { "Hello World!" }))
             .service(greet)
     })
